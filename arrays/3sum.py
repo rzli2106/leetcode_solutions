f1439652971247ef2119a41043461920aa07f098
Richard Li <rzli2106@gmail.com>
2026-09-16 23:06:33 -0400

    Initial commit

---
+++ arrays/3sum.py
@@ -0,0 +1,23 @@
+#Brute force method has O(n^3) which exceeds time limit
+'''class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                for k in range(j+1, len(nums)):
+                    if k > j+1 and nums[k] == nums[k-1]:
+                        continue
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        triplet = [nums[i], nums[j], nums[k]]
+                        result.append(triplet)
+        return result'''
