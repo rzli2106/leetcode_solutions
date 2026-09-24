d34772ee12d6932931d57a5d4b97239f937f22e3
Richard Li <rzli2106@gmail.com>
2026-09-24 00:59:41 -0400

    adding base case of empty list of strings

--- arrays/longest_common_prefix.py
+++ arrays/longest_common_prefix.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        if not strs:
+            return ""
+
         result = ''
         for i in range(len(strs[0])):
             for s in strs:
                 if i == len(s) or s[i] != strs[0][i]:
                     return result
             result += s[i]
-            
+
         return result
         
