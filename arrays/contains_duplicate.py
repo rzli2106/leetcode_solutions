d35ff63d7ce55d92d33a141537385ec09d1f2a99
Richard Li <rzli2106@gmail.com>
2026-09-24 00:34:42 -0400

    initial commit

---
+++ arrays/contains_duplicate.py
@@ -0,0 +1,30 @@
+'''
+Original thought: time error
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        
+        return False'''
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) <= 1:
+            return False
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        
+        return False
+            
